d838356d0adf2e588ed66a9fe2429661b7e85576
kokobd <contact@zelinf.net>
2022-12-20 23:08:52 +0800

    wip

--- flake.nix
+++ flake.nix
@@ -8,62 +8,65 @@
 
   outputs = inputs@{ self, nixpkgs, flake-utils }:
     with flake-utils.lib;
-    let
-      settings = import ./settings.nix;
-      mkClient = system: name: path:
-        let
-          client = import path
-            {
-              pkgs = nixpkgs.legacyPackages."${system}";
-              inherit settings;
-            };
-        in
-        {
-          type = "app";
-          program = "${client}/bin/${name}";
-        };
-    in
     {
-      nixosConfigurations =
+      lib = settings:
         {
-          gateway-hyperV = nixpkgs.lib.nixosSystem {
-            system = system.x86_64-linux;
-            specialArgs = { inherit settings; };
-            modules = [ ./gateway/machines/hyper-v.nix ];
+          nixosConfigurations =
+            {
+              gateway-hyperV = nixpkgs.lib.nixosSystem {
+                system = system.x86_64-linux;
+                specialArgs = { inherit settings; };
+                modules = [ ./gateway/machines/hyper-v.nix ];
+              };
+
+              gateway-respberryPi3BP = nixpkgs.lib.nixosSystem {
+                system = system.aarch64-linux;
+                specialArgs = { inherit settings; };
+                modules = [ ./gateway/machines/raspberryPi3BP.nix ];
+              };
+            };
+          nixosModules = {
+            gateway = import ./gateway/v2ray.nix;
           };
+        } //
+        eachDefaultSystem (system:
+          let
+            mkClient = system: name: path:
+              let
+                client = import path
+                  {
+                    pkgs = nixpkgs.legacyPackages."${system}";
+                    inherit settings;
+                  };
+              in
+              {
+                type = "app";
+                program = "${client}/bin/${name}";
+              };
+          in
+          {
+            packages =
+              let
+                params = {
+                  pkgs = nixpkgs.legacyPackages.${system};
+                  nixos = nixpkgs.lib.nixosSystem;
+                  system = system;
+                  inherit settings;
+                };
+              in
+              {
+                server = import ./server params;
 
-          gateway-respberryPi3BP = nixpkgs.lib.nixosSystem {
-            system = system.aarch64-linux;
-            specialArgs = { inherit settings; };
-            modules = [ ./gateway/machines/raspberryPi3BP.nix ];
-          };
-        };
-      nixosModules = {
-        gateway = import ./gateway/v2ray.nix;
-      };
+                server-no-cn = import ./server/no-cn.nix params;
 
-      packages."${system.x86_64-linux}" =
-        let
-          params = {
-            pkgs = nixpkgs.legacyPackages."${system.x86_64-linux}";
-            nixos = nixpkgs.lib.nixosSystem;
-            system = system.x86_64-linux;
-            inherit settings;
-          };
-        in
-        {
-          server = import ./server params;
+                gateway = import ./gateway/env.nix params;
 
-          server-no-cn = import ./server/no-cn.nix params;
-
-          transit = import ./transit params;
-        };
-
-      apps."${system.x86_64-linux}" = {
-        client = mkClient system.x86_64-linux "v2ray" ./client;
-      };
-      apps."${system.aarch64-darwin}" = {
-        client = mkClient system.aarch64-darwin "v2ray" ./client;
-      };
+                transit = import ./transit params;
+              };
+            apps = {
+              client = mkClient system "v2ray" ./client;
+            };
+          }
+        );
     };
 }
---
+++ gateway/env.nix
@@ -0,0 +1,20 @@
+{ pkgs
+, nixos
+, system
+, settings
+}:
+
+let
+  eval = nixos {
+    inherit system;
+    modules = [ ./v2ray.nix ];
+    specialArgs = { inherit settings; };
+  };
+in
+pkgs.buildEnv {
+  name = "v2ray";
+  paths = [
+    eval.config.systemd.units."v2ray.service".unit
+    eval.config.systemd.units."firewall.service".unit
+  ];
+}
